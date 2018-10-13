06efff2efcfc21efe8821deaaed5642ef3a516b0
Juan Bedoya <juansb827@gmail.com>
2018-10-13 18:16:53 -0500

    Redux advanced - dne

--- redux-01-start/src/store/reducers/counter.js
+++ redux-01-start/src/store/reducers/counter.js
@@ -1,35 +1,21 @@
-import * as actionsTypes from '../actions';
+import * as actionsTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
-    counter: 0    
+    counter: 0
 };
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionsTypes.INCREMENT:
-            return {
-                ...state,
-                counter: state.counter + 1
-            }
-            break;
+            return updateObject(state, { counter: state.counter + 1 })
         case actionsTypes.DECREMENT:
-            return {
-                ...state,
-                counter: state.counter - 1
-            }
-            break;
+            return updateObject(state, { counter: state.counter - 1 })
         case actionsTypes.ADD:
-            return {
-                ...state,
-                counter: state.counter + action.value
-            }
-            break;
+            return updateObject(state, { counter: state.counter + action.value })
         case actionsTypes.SUBTRACT:
-            return {
-                ...state,
-                counter: state.counter - action.value
-            }
-            break;
+            return updateObject(state, { counter: state.counter - action.value });
+
     }
 
     return state;
--- redux-01-start/src/store/reducers/result.js
+++ redux-01-start/src/store/reducers/result.js
@@ -1,27 +1,20 @@
-import * as actionsTypes from '../actions';
-
-const initialState = {    
+import * as actionsTypes from '../actions/actionTypes';
+import { updateObject } from '../utility'
+const initialState = {
     results: []
 };
 
+const deleteResult = (state, action) => {
+    const updatedArray = state.results.filter(el => el.id !== action.resultId);
+    return updateObject(state, { results: updatedArray });
+};
+
 const reducer = (state = initialState, action) => {
-    switch (action.type) {       
+    switch (action.type) {
         case actionsTypes.STORE_RESULT:
-            return {
-                ...state,
-                results: state.results.concat({id: new Date(), value: action.result})
-            }            
+            return updateObject(state, { results: state.results.concat({ id: new Date(), value: action.result }) });
         case actionsTypes.DELETE_RESULT:
-            /*const id = 2;
-            const newArray = [...state.results];
-            newArray.splice(id, 1); */
-            const updatedArray = state.results.filter(el => el.id !== action.resultId );
-            return {
-                ...state,
-                results: updatedArray
-            }
-
-
+            return updateObject(state, action);
     }
 
     return state;
