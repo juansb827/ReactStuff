7f87dc2e3ec5701ef7ac9c9b076253091d0259df
Juan <juansb827@gmail.com>
2019-08-07 20:26:16 -0500

    Used useState and useEffect

--- hooks-01-starting-project/src/components/Ingredients/Ingredients.js
+++ hooks-01-starting-project/src/components/Ingredients/Ingredients.js
@@ -1,16 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
 import Search from './Search';
-
+import IngredientList from './IngredientList';
+const URL = 'https://react-hooks-sample-999999.firebaseio.com/ingredients.json';
 function Ingredients() {
+  const [ingredients, setIngredients] = useState([]);
+  
+  useEffect(() => {
+    fetch(URL)
+    .then(response => response.json())
+    .then(responseData => {
+      const fetchedIngredients = [];
+      for (const key in responseData) {
+        fetchedIngredients.push({
+          id: key,
+          title: responseData[key].title,
+          amount: responseData[key].amount
+        });
+      }
+      setIngredients(fetchedIngredients); 
+    }); 
+  }, []);
+
+  const ingredientAddedHandler = ingredient => {
+    fetch(URL,
+      {
+        method: 'POST',
+        body: JSON.stringify(ingredient),
+        headers: { 'Content-Type': 'application/json' }
+      })
+        .then(response => response.json())
+        .then(responseData => {
+          setIngredients(prevIngredients => [...prevIngredients, {
+            ...ingredient,
+            id:  responseData.name
+          }]);
+        })
+
+  }
+
+  const onIngredientRemovedHandler = id => {
+    setIngredients(prevIngredients => prevIngredients.filter(ingredient => ingredient.id !== id));
+  }
+
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onIngredientAdded={ingredientAddedHandler} />
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList ingredients={ingredients} onRemoveItem={onIngredientRemovedHandler} />
       </section>
     </div>
   );
